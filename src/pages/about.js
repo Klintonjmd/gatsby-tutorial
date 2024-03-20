80fca70ee4f6e8c633ff2444e9db2f2ee91c8a98
Klintonjmd <100642525+Klintonjmd@users.noreply.github.com>
2024-03-20 15:55:46 -0400

    Create about.js and update index.js

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>I am creating this site in preparation for my interview tomorrow (3/21/24) with "Unite Us" for a software engineering role.</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+// Step 3: Export your component
+export default AboutPage
