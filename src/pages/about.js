28e8e4af0b3b44d74c1934819461c9bfaa934c69
Klintonjmd <100642525+Klintonjmd@users.noreply.github.com>
2024-03-20 15:57:01 -0400

    Add links to each page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,16 @@
-// Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
 
-// Step 2: Define your component
 const AboutPage = () => {
   return (
     <main>
       <h1>About Me</h1>
-      <p>I am creating this site in preparation for my interview tomorrow (3/21/24) with "Unite Us" for a software engineering role.</p>
+      <Link to="/">Back to Home</Link>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </main>
   )
 }
 
 export const Head = () => <title>About Me</title>
-// Step 3: Export your component
+
 export default AboutPage
